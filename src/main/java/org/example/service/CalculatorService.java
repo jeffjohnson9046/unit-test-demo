6bf099a34e30b0f8a83a552caa35b4cc42f8635a
Jeff Johnson <jeffjohnson9046@hotmail.com>
2020-02-19 09:53:24 -0800

    Minor refactoring; additional documentation

--- src/main/java/org/example/service/CalculatorService.java
+++ src/main/java/org/example/service/CalculatorService.java
@@ -1,8 +1,42 @@
 package org.example.service;
 
+/**
+ * A service for providing the basic four-function calculator.
+ */
 public interface CalculatorService {
-    double add(double input1, double input2);
-    double subtract(double input1, double input2);
-    double multiply(double input1, double input2);
-    double divide(double input1, double input2);
+    /**
+     * Add two numbers together.
+     *
+     * @param addend1 The first number to add
+     * @param addend2 The second number to add
+     * @return A {@code double} representing the sum of {@code addend1} and {@code addend2}
+     */
+    double add(double addend1, double addend2);
+
+    /**
+     * Subtract one number from another.
+     *
+     * @param minuend The number being subtracted from
+     * @param subtrahend the number to subtract from the minuend
+     * @return A {@code double} representing the difference between the {@code minuend} and {@code subtrahend}
+     */
+    double subtract(double minuend, double subtrahend);
+
+    /**
+     * Multiply two numbers together.
+     *
+     * @param factor1 The first number to multiply
+     * @param factor2 The second number to multiply
+     * @return A {@code double} representing the product of {@code factor1} and {@code factor2}
+     */
+    double multiply(double factor1, double factor2);
+
+    /**
+     * Divide one number by another.
+     *
+     * @param dividend The number to divide
+     * @param divisor The number to divide by
+     * @return A {@code double} representing the quotient of dividing {@code dividend} by the {@code divisor}
+     */
+    double divide(double dividend, double divisor);
 }
